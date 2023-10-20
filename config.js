141bf13287b9be891d57b0b7cab36c23883c6ad5
thuandz321 <121750974+thuandz321@users.noreply.github.com>
2023-10-20 17:39:05 +0700

    add

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Chị';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Chị Ánh <3';
+const contentCard = 'Kiếm người yêu chúc đi chứ có cái nịt :))) #thuan';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = '336569552_914344756472705_3425903769626861343_n.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
